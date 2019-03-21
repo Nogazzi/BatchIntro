03d4223e2d82d0de670d7364b12cca8a380a82d5
Pawel.Noganski <nogazzi@gmail.com>
2019-03-21 15:29:38 +0100

    Initial commit

---
+++ src/test/java/com/example/BatchIntro/BatchIntroApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.BatchIntro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = BatchIntroApplication.class)
+public class BatchIntroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
